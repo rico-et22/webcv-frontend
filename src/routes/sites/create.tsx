357df795825fcedbc13486f957d5572f683e660f
Kamil Pawlak <hello@kamilpawlak.com>
2026-05-13 22:30:57 +0200

    feat: make form layout more breathable - create site

--- src/routes/sites/create.tsx
+++ src/routes/sites/create.tsx
@@ -121,7 +121,7 @@ function CreateSitePage() {
           <form
             id="site-form"
             onSubmit={handleSubmit(onSubmit)}
-            className="w-full overflow-y-auto p-6 lg:w-[500px] lg:shrink-0 lg:border-r lg:border-border/60"
+            className="w-full overflow-y-auto p-6 lg:w-[500px] xl:w-[600px] 2xl:w-[700px] lg:shrink-0 lg:border-r lg:border-border/60"
           >
             <SiteForm onAiApply={handleAiApply} />
           </form>
